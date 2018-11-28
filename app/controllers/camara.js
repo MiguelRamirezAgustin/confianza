979aac37eec6c3a492a689279c8444a90d5d9e7e
MiguelRamirezAgustin <miguel.r.agustin@gmail.com>
2018-11-28 10:43:09 -0600

    pantalla para infromacionGeneral y cambios de estilos

--- app/controllers/camara.js
+++ app/controllers/camara.js
@@ -16,43 +16,48 @@ var grados;
 //control que vizualiza la foto tomada o imagen seleccionada
 viewImage = Ti.UI.createImageView({
 	borderColor : 'white',
-	top : 30,
+	top : 20,
 	width : '350dp',
 	height : '450dp', //480
 	borderRadius : 5,
 	borderWidth : 2,
 	layout : 'vertical'
 });
-$.container.add(viewImage);
-
-//se agrega boton para seleccionar imagen de la galeria
-btnGaleria = Ti.UI.createButton({
-	bottom : 10,
-	right : 5,
-	width : '120px',
-	height : '120px',
-	backgroundImage : '/images/picture.png'
-});
-$.footer.add(btnGaleria);
+$.header.add(viewImage);
 
 //se agrega boton para tomar una foto
 btnFoto = Ti.UI.createButton({
-	bottom : 10,
-	left : 10,
-	width : '120px',
-	height : '120px',
+	top : 10,
+	left : "20dp",
+	width : '11%',
+	height : '70%',
 	backgroundImage : '/images/camera.png'
 });
 $.footer.add(btnFoto);
 
+
 //se agrego boton para enviar la foto al servicioSAD
 btnEnvio = Ti.UI.createButton({
-	bottom : 10,
+	top : 20,
 	backgroundColor : '#6e27c5',
 	title : 'Enviar foto',
-	width : '180dp'
+	title : 'Enviar Imágen',
+	width : '43%',
+	height:"50%",
+	left:"33dp",
+	borderRadius:3
 });
 $.footer.add(btnEnvio);
+
+//se agrega boton para seleccionar imagen de la galeria
+btnGaleria = Ti.UI.createButton({
+	top: 15,
+	left:"25dp",
+	width : '12%',
+	height : '55%',
+	backgroundImage : '/images/picture.png'
+});
+$.footer.add(btnGaleria);
 
 btnGaleria.addEventListener('click', function(e) {
 	abrirGaleria();
@@ -107,6 +112,16 @@ function abrirFoto() {
 	});
 }
 
+	//Notificacion
+	function notificacion(){
+		var notifi=Ti.UI.createNotification({
+		 message:"Enviando Imagen",
+		 duration:Ti.UI.NOTIFICATION_DURATION_LONG,
+		 gravity:Titanium.UI.Android.GRAVITY_CLIP_VERTICAL
+		});
+		notifi.show();
+	}
+
 btnEnvio.addEventListener('click', function(e) {
 	//verificar conexion a internet
 	if (Titanium.Network.networkType == Titanium.Network.NETWORK_NONE) {
@@ -118,10 +133,13 @@ btnEnvio.addEventListener('click', function(e) {
 	if (seleccionoImagen == false) {
 		alert('Seleccione una imagen o tome una Foto!');
 	} else {
+        // boton 
+		btnEnvio.title = "Procesando...";
+		btnEnvio.color = '#1fc0d4';		
+		btnEnvio.enabled = false;
+		// llamada de notificacion
+		notificacion();
 
-		// btnEnvio.title = "Procesando...";
-		// btnEnvio.color = 'white';
-		// btnEnvio.enabled = false;
 		
 		var url = 'https://8oepupymf6.execute-api.us-east-2.amazonaws.com/sprint2-test';
 		
@@ -138,6 +156,10 @@ btnEnvio.addEventListener('click', function(e) {
 				var miObjetoRespuesta = {
 					response : JSON.parse(this.responseText)
 				};
+				//habilitar boton al responder el servicio
+				btnEnvio.title = "Enviar Imágen";
+				btnEnvio.color = 'white';
+				btnEnvio.enabled = true;
 
 				///Ti.App.Properties.setString('objetosService', miObjetoRespuesta);
 				var info = Alloy.createController('info', miObjetoRespuesta).getView();
@@ -151,6 +173,11 @@ btnEnvio.addEventListener('click', function(e) {
 			},
 			onerror : function(e) {
 				alert('Error al enviar la imagen volver a intentar!');
+				//habilita el boton al responder el servicio con error
+				btnEnvio.title = "Enviar Imágen";
+				btnEnvio.color = 'white';
+				btnEnvio.enabled = true;
+
 			},
 			timeout : 20000,
 		});
@@ -185,10 +212,10 @@ var etiquetas = Ti.UI.createLabel({
 	},
 	width : Ti.UI.SIZE,
 	height : Ti.UI.SIZE,
-	top : 5,
-	color : 'white'
+	color : 'white',
+	top:"15dp"
 });
-$.header.add(etiquetas);
+$.container.add(etiquetas);
 
 etiquetas.addEventListener('click', function(e) {
 	//alert('alerta');
@@ -199,7 +226,7 @@ etiquetas.addEventListener('click', function(e) {
 function rotarImage() {
 	grados = Number(grados) + 90;
 	var rotarImage = ImageFactory.imageWithRotation(image, {
-		degrees : 90,
+		degrees : -90,
 	});
 	image = rotarImage;
 	viewImage.setImage(image);
